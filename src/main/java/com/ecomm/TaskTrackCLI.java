b4139209511f1b8f8705516e2676fe1488a75177
ERRAZI <errazi111@gmail.com>
2025-05-19 16:10:50 +0100

    first commit

---
+++ src/main/java/com/ecomm/TaskTrackCLI.java
@@ -0,0 +1,13 @@
+package com.ecomm;
+
+import java.util.Arrays;
+
+public class TaskTrackCLI {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To TaskTrack");
+
+    }
+
+
+}
